289d666c1bc227adbc307ab2f7350930c6d6ee34
ripplefox <lr@ripplefox.com>
2021-08-31 14:05:26 +0800

    XAG Ledger tool

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# xag-ledgerstats
+# XAG Ledger Stats
+
+You can use this tool to fetch all the account balance information or a specific XAG ledger number.
+
+The default server it will connect to is `g1.xrpgen.com`.
+
+## Workflow
+
+You'll need to have [node (nodejs)](https://nodejs.org/en/download/) installed on your computer.
+
+After clone this repository, install dependencies:
+
+`npm install`
+
+## Syntax
+
+### Fetch ledger account data into .json file
+
+Run the code to fetch all account balances using:
+
+```
+npm run fetch
+```
+
+If you don't append any arguments, the latest (closed) ledger will be fetched from the public `g1.xrpgen.com` server.
+
+If you want to fetch a specific ledger, append the ledger index:
+
+```
+npm run fetch 22508618
+```
+
+**The tool will save the balance information for the ledger in `./data/ledgerno.json`, eg. `./data/22508618.json`.
+
+### Process ledger data from .json file (to stats)
+
+Run:
+
+```
+npm run stats 22508618
+```
+
+... where `22508618` is the ledger index you want to process. You should -of course- have fetched this ledger first.
+
+Sample:
+
+```
+ -- Ledger close time:   2021-Aug-31 05:51:02.000000000
+Reading ledger stats: 22508618.json
+ -- Accounts:              82600
+ -- Ledger close time:     2021-Aug-31 05:54:42.000000000
+ -- Ledger hash:           5396F8478976A4083A17BAA22F4141B284146ACB349626BE11252C8A337D27D0
+ -- Ledger index:          22508618
+ -- Total XAG existing:    99,999,998,065.321472
+```
---
+++ parseLedgerFile.js
@@ -0,0 +1,141 @@
+process.stdout.write('\033c')
+
+const numeral = require('numeral')
+const fs = require('fs')
+const { table } = require('table')
+
+const instructions = () => {
+    console.log('Error: please enter the (fetched) ledger number (after the command to execute)')
+    console.log('You can find the ledger number by checking the ./data/ folder (without the `.json` part)')
+    console.log('')
+    console.log('  eg. npm run stats 32570')
+    console.log('')
+    console.log('If you didn\'t fetch this ledger data first:')
+    console.log('  npm run fetch')
+    console.log('')
+    process.exit(0)
+}
+
+if (process.argv.length < 3) {
+    instructions()
+}
+
+let filename = parseInt(process.argv[2].split('.')[0]) + '.json'
+let exportJson = {
+    meta: {},
+    top100Balance: 0,
+    accountPercentageBalance: [],
+    accountNumberBalanceRange: []
+}
+if (fs.existsSync(__dirname + '/data/' + filename)) {
+    console.log('Reading ledger stats:', filename)
+    let data = JSON.parse(fs.readFileSync(__dirname + '/data/' + filename))
+    data.balances.sort((a, b) => { return (a.b > b.b) ? -1 : ((b.b > a.b) ? 1 : 0) })
+    
+    let numberOfAccounts = data.balances.length
+    console.log(' -- Accounts:             ', numberOfAccounts)
+    exportJson.meta.numberAccounts = numberOfAccounts
+    console.log(' -- Ledger close time:    ', data.stats.close_time_human)
+    exportJson.meta.ledgerClosedAt = data.stats.close_time_human
+    console.log(' -- Ledger hash:          ', data.stats.hash)
+    exportJson.meta.ledgerHash = data.stats.hash
+    console.log(' -- Ledger index:         ', data.stats.ledger_index)
+    exportJson.meta.ledgerIndex = data.stats.ledger_index
+    console.log(' -- Total XAG existing:   ', numeral(data.stats.total_coins).format('0,0.000000'))
+    exportJson.meta.existingXRP = data.stats.total_coins
+    console.log('')
+    console.log('Calculating spendable XAG sum')
+    let balanceSum = data.balances.reduce((a, b) => { return a + b.b }, 0)
+    console.log(' -- Accounts balance sum: ', numeral(balanceSum).format('0,0.000000'))
+    exportJson.top100Balance = balanceSum
+    console.log('')
+
+    console.log('Stats 🎉')
+    console.log('')
+    console.log(' -- Top 100')
+    let top100sum = data.balances.slice(0, 100).reduce((a, b) => { return a + b.b }, 0)
+    console.log('  > Balance sum                                  ', numeral(top100sum).format('0,0.000000'))
+    console.log('  > Balance percentage (vs. Total XAG existing)  ', numeral(top100sum / data.stats.total_coins * 100).format('0.00') + ' %')
+    console.log('  > Balance percentage (vs. Accounts balance sum)', numeral(top100sum / balanceSum * 100).format('0.00') + ' %')
+
+    console.log('')
+    console.log(' -- Percentage of accounts with balance starting at...')
+    let percentages = [ 0.01, 0.1, 0.2, 0.5, 1, 2, 3, 4, 5, 10 ]
+    let pctAccountsBalance = [ [ 'Percentage', '# Accounts', 'Balance equals (or greater than)' ] ]
+    percentages.forEach(p => {
+        let n = Math.round(numberOfAccounts / 100 * p)
+        let e = data.balances.slice(0, n).reverse()[0].b
+        pctAccountsBalance.push([ 
+            p + ' %', 
+            numeral(n).format('0,0.'), 
+            numeral(e).format('0,0.000000') + ' XAG'
+        ])
+        exportJson.accountPercentageBalance.push({
+            percentage: p,
+            numberAccounts: n,
+            balanceEqGt: e
+        })
+    })
+    console.log(table(pctAccountsBalance, { 
+        columns: {
+            0: { alignment: 'right' },
+            1: { alignment: 'right' },
+            2: { alignment: 'right' }
+        }
+    }))
+    // numberOfAccounts
+
+    console.log('')
+    console.log(' -- Number of accounts and sum of balance range')
+    let balanceranges = [ 1000000000, 500000000, 100000000, 20000000, 10000000, 5000000, 1000000, 500000, 100000, 75000, 50000, 25000, 10000, 5000, 1000, 500, 20, 0 ]
+    let noAccountsBalanceRange = [ [ '# Accounts', 'Balance from', '... To', 'Sum (XAG)' ] ]
+    let sliceFrom = 0
+    let lastBalanceRange = 0
+    let XbalanceSum = 0
+    balanceranges.forEach(b => {
+        let a = 0
+        let f = b
+        let t = lastBalanceRange
+        let s = 0
+        for (let i = sliceFrom; i < numberOfAccounts; i++) {
+            if (data.balances[i].b < f) {
+                sliceFrom = i
+                break;
+            } else {
+                s += data.balances[i].b
+                a++
+            }
+        }
+        noAccountsBalanceRange.push([ 
+            numeral(a).format('0,0.'),
+            numeral(f).format('0,0.'),
+            lastBalanceRange === 0 ? '∞' : numeral(t).format('0,0.'),
+            numeral(Math.round(s)).format('0,0.000000')
+        ])
+        exportJson.accountNumberBalanceRange.push({
+            numberAccounts: a,
+            balanceFrom: f,
+            balanceTo: t,
+            balanceSum: s
+        })
+        lastBalanceRange = b
+    })
+    console.log(table(noAccountsBalanceRange, { 
+        columns: {
+            0: { alignment: 'right' },
+            1: { alignment: 'right' },
+            3: { alignment: 'right' }
+        }
+    }))
+    console.log('')
+
+    console.log('')
+    console.log('---')
+    console.log('Writing stats...')
+    let exportFilename = __dirname + '/data/' + filename.replace(/.json$/, '.stats.json')
+    fs.writeFileSync(exportFilename, JSON.stringify(exportJson), 'utf8');
+    console.log(' > Stats written as JSON object to: ', exportFilename)
+    console.log('')
+} else {
+    instructions()
+}
